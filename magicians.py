51e1dbeb0e559da677883b30019917c7da2be152
Jameson <jamesonbos33@gmail.com>
2024-03-12 23:18:43 -0600

    .

--- magicians.py
+++ magicians.py
@@ -2,3 +2,5 @@ magicians = ['jameson', 'emily', 'logan', 'bailey']
 
 for magician in magicians:
     print(f"{magician.title()}, that was a great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}")
+print("\nThank you, everyone, that was a great show!")
