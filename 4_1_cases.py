36fd5d5a979104d14a1b625f42b0b9552c2e9a78
Jameson <jamesonbos33@gmail.com>
2024-03-12 23:22:41 -0600

    .

--- 4_1_cases.py
+++ 4_1_cases.py
@@ -4,3 +4,10 @@ Modify your for loop to print a sentence using the name of the pizza, instead of
 For each pizza, you should have one line of output containing a simple statement like I like pepperoni pizza.
 Add a line at the end of your program, outside the for loop, that states how much you like pizza. 
 The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!'''
+
+
+pizza_names = ['pepperoni', 'cheese', 'pineapple']
+
+for pizza in pizza_names:
+    print(f"I like {pizza} pizza.")
+print("\nI do like me my pizza...")
